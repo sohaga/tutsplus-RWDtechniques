39126867e04523d4137984ba1bf8f85ba05b505b
mehedi4475 <mehedi4475@gmail.com>
2015-04-13 16:57:23 +0600

    One page responsive design

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-	var n = "#nav", no = "nav-open";
+	/*
+    var n = "#nav", no = "nav-open";
     $("#nav-menu").click(function(){
         if($(n).hasClass(no)){
            // $(n).removeClass(no);
@@ -21,5 +22,45 @@ $(document).ready(function(){
             }, 320);
         }
     });
+    */
+    
+    
+    var n = "#nav", no = "nav-open";
+    
+    $(".nav-menu").click(function(){
+        if($(n).hasClass(no)){
+           $(n).animate({height:0},300);
+            
+            setTimeout(function(){
+                 $(n).removeClass(no).removeAttr('style'); 
+            },320)
+        }
+        else{
+            var newH = $(n).css('height','auto').height();
+            $(n).height('0').animate({height:newH}, 300);
+            
+            setTimeout(function(){
+                $(n).addClass(no).removeAttr('style');
+            }, 320)
+           
+            
+        }
+    
+    });
+    
+    
+    
+    //portfolio
+    $('.portfolio').mixItUp({
+    animation: {
+		queueLimit: 6
+	}
+    });
+    
+    //fancybox
+    $(".fancybox").fancybox({
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
     
 });
